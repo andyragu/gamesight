7cc8c98470a0de44bacc60763b794cd10638b0b1
Star <starlinjimenez05@gmail.com>
2023-09-06 20:41:40 -0400

    Setup express.js

---
+++ express/app.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app =express();
+
+app.get('/',(req,res) => {
+    res.send("jake and andy are cool");
+});
+
+app.listen(4000, () =>{
+    console.log("listening to port 4000")
+});
