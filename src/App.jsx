cb5f65e0f6c69340847e11f882d208415e413845
Andres Agudelo <andyagudelo42@gmail.com>
2023-09-25 02:34:33 -0400

    About page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Register } from "./Components/userCreation/Register";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import MainPage from "./Components/pages/MainPage";
 import LoginPage from "./Components/pages/LoginPage";
+import AboutPage from "./Components/pages/AboutPage";
 
 function App() {
     /*const [currentForm, setCurrentForm] = useState('main');  // Default to main page
@@ -31,6 +32,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<MainPage />}/>
                     <Route path="/login" element={<LoginPage />}/>
+                    <Route path= "/about" element={<AboutPage />}/>
                 </Routes>   
             </main>
         </BrowserRouter>
