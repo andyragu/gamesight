68a43b0f4f4e968ad78aa04435003c0e4887780e
Andres Agudelo <andyagudelo42@gmail.com>
2023-09-30 01:54:19 -0400

    some steam design

--- src/Components/pages/MainPage.jsx
+++ src/Components/pages/MainPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './MainPage.css';
 import SteamIDInput from '../userCreation/SteamIDInput';
 import { NavLink } from 'react-router-dom';
+import myImage from '../steamicon/steam-logo-white-removebg-preview.png';
 
 function MainPage(props) {
+    const sectionTwoRef = useRef(null);
+
+    const scrollToSectionTwo = () => {
+        sectionTwoRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    useEffect(() => {
+    }, []);
+
     return (
         <div className="container">
             <section className="one">
@@ -13,33 +23,27 @@ function MainPage(props) {
                         <NavLink to="/about" style={{ textDecoration: 'none' }}>
                             <div className="about-option">about</div>
                         </NavLink>
-                        <NavLink to="/about" style={{ textDecoration: 'none' }}>
+                        <NavLink to="/" style={{ textDecoration: 'none' }}>
                             <div className="howitworks-option">how it works</div>
                         </NavLink>
                         <NavLink to="/login" style={{ textDecoration: 'none' }}>
+                            <img src={myImage} alt="Steam Image" className="steam-image" width="20" height="20" />
                             <div className="login-option">login</div>
                         </NavLink>
                     </div>
                 </div>
                 <div className="middle-content">
                     <h2>SEE YOUR NEXT GAME</h2>
-                    <NavLink to="/discover">
-                        <button>DISCOVER</button>
-                    </NavLink>
-                    <NavLink to="/SteamID">
-                        <button>SteamID</button>
-                    </NavLink>
+                                
+                    <button onClick={scrollToSectionTwo}>DISCOVER</button>
+
                 </div>
             </section>
 
-            <section className="two">
+            <section id="two" ref={sectionTwoRef}>
                 <div className="page-name">
                     <h2>Discover</h2>
                 </div>
-            </section>
-
-            <section className="three">
-
             </section>
         </div>
     );
