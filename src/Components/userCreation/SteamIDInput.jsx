ba4b25b989b33f1b39f1433710c03cbb230331db
Jake <123135256+minionbytes@users.noreply.github.com>
2023-09-30 01:34:38 -0400

    Leveled Header

--- src/Components/userCreation/SteamIDInput.jsx
+++ src/Components/userCreation/SteamIDInput.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+<<<<<<< Updated upstream
 
 
 function SteamIDInput() {
@@ -8,10 +9,20 @@ function SteamIDInput() {
   const handleChange = (e) => {
     const steamIDValue = e.target.value
     setsteamID({steamID: steamIDValue})
+=======
+
+function SteamIDInput() {
+  const [steamID, setsteamID] = useState("");
+
+  const handleChange = (e) => {
+    const steamIDValue = e.target.value;
+    setsteamID(steamIDValue);
+>>>>>>> Stashed changes
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
+<<<<<<< Updated upstream
     const steamIDData = { steamID: steamID };
     axios
     .post("http://localhost:3001/steamID", steamIDData).then((result) => {
@@ -31,6 +42,32 @@ function SteamIDInput() {
       </form>
     </div>
     </>
+=======
+    const steamIDData = { steamID };
+    axios
+      .post("http://localhost:3001/steamID", steamIDData)
+      .then((result) => {
+        console.log(result.status, result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <div className="input-container">
+      <form onSubmit={handleSubmit}>
+        <input 
+          onChange={handleChange}
+          type="text"
+          name="steamID"
+          placeholder="Insert steamID"
+          className="steamID-input"
+        />
+        <button type="submit" className="submit-btn">Submit</button>
+      </form>
+    </div>
+>>>>>>> Stashed changes
   );
 }
 
