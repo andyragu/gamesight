023a3c855be8d573c06772023c519364ecdf0c0e
Andres Agudelo <andyagudelo42@gmail.com>
2023-09-30 02:45:20 -0400

    working on steam login

--- src/Components/pages/LoginPage.jsx
+++ src/Components/pages/LoginPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Login } from '../userCreation/Login'
 import './LoginPage.css'; 
 import { NavLink } from 'react-router-dom';
 
@@ -7,12 +6,16 @@ function LoginPage() {
 
   return (
       <div className='LoginPage'>
-        <NavLink to="/" style={{ textDecoration: 'none' }}>
-          <div className="header">
-            Gamesight
+          <NavLink to="/" style={{ textDecoration: 'none' }}>
+            <div className="login-header">
+              Gamesight
+            </div>
+          </NavLink>
+          <div className="login-box">
+                <h2 className="login-title">Logging in to gamesight with your Steam allows you to get the full experience of discovering games</h2>
+                <h2 className="login-explanation">To provide a better service we will fetch public information about your account from the Steam Web API (this includes steamid, profile name, avatar, and game list). This website is not affiliated with Valve Corporation or Steam. You can delete your account and all of the data at any time.</h2>
           </div>
-        </NavLink>
-        <Login/>
+
       </div>
   )
 }
