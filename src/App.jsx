735de5d6789679831d10fd556a31a65bacf4ba40
Andres Agudelo <andyagudelo42@gmail.com>
2023-10-02 02:36:13 -0400

    Some more changes to steam login mostly styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Login } from "./Components/userCreation/Login";
-import { Register } from "./Components/userCreation/Register";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import MainPage from "./Components/pages/MainPage";
 import LoginPage from "./Components/pages/LoginPage";
--- src/Components/pages/LoginPage.jsx
+++ src/Components/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './LoginPage.css'; 
 import { NavLink } from 'react-router-dom';
+import myImage from '../steamicon/steam-logo-white-removebg-preview.png';
 
 function LoginPage() {
 
@@ -14,6 +15,10 @@ function LoginPage() {
           <div className="login-box">
                 <h2 className="login-title">Logging in to gamesight with your Steam allows you to get the full experience of discovering games</h2>
                 <h2 className="login-explanation">To provide a better service we will fetch public information about your account from the Steam Web API (this includes steamid, profile name, avatar, and game list). This website is not affiliated with Valve Corporation or Steam. You can delete your account and all of the data at any time.</h2>
+                <button id="login-steam">
+                  <img src={myImage} alt="Steam-Image2" className="steam-image2" width="20" height="20"/>
+                  Login with Steam
+                </button>
           </div>
 
       </div>
